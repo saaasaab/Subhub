7d42fdedfdb6f6d39d3b2754bbbc9f7e17ca860a
Scott Laughlin <scott.laughlin@yourbrandlive.com>
2021-10-13 20:32:28 -0700

    Adding to subhub

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import {subData} from './data/subData';
 import './App.scss';
 import SubscriptionCardElement from './components/SubscriptionCardElement';
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import useFetch from "react-fetch-hook"
+
 
 function App() {
+  const [categoryData, setCategoryData] = useState({})
+  // [ ] Selected Main Category
+  // [ ] Selected Sub Categoty
+  // [ ] When select Main Category, sub category loads
+  // [ ] Default selected is the Most Popular
+
+  
+  let url = "http://localhost:8080/get-categories";
+
+
+  useEffect(()=>{
+    getCategoryData();
+  },[])
+
+
+  const getCategoryData = () => {
+    axios.get(url).then((res) => {
+      const allData = res.data;
+      setCategoryData(allData)
+    }).catch(error => console.error(`Error: ${error}`))
+  }
   
   return (
     <div className="App">
-   
+      <nav>
+       
+        {
+          
+        Object.keys(categoryData).map(function(key, index) {
+          console.log(key,categoryData[key],index)
+          return (
+            <span key={key}>{key}</span>
+          )
+        })
 
-   
+        }
+      
+      </nav>
       <div className="sub-list">
         {subData.map((e) => <SubscriptionCardElement key={e.id} subData={e}/>)} 
-        {/* isHover={isHover} onHover={setIsHover} */}
       </div>
     </div>
 
--- src/components/SubscriptionCardElement.jsx
+++ src/components/SubscriptionCardElement.jsx
@@ -1,25 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import disneyPlus from '../data/images/disneyPlus.jpeg'
+import axios from "axios"
+
+// const fetchData = () => {
+//     return axios.get("http://localhost:8080")
+//         .then((response) => console.log(response.data));
+// }
 
 function SubscriptionCardElement(props) {
     const { subData } = props;
     const [isHover, setHover] = useState(false);
+
+
+    // useEffect(() => {
+    //     fetchData();
+    // }, []);
+    
+
+
     return (
-        <>
-            {!isHover ? 
-            <div className="subcard"
-                onMouseEnter={() => { setHover(true); console.log("ENTER") }}
-                // onMouseLeave={() => { setHover(false); console.log("LEAVE") }}
-                >
+        <div className="subcard">
+
+            <div className="subcard--init"
+            // onMouseEnter={() => { setHover(true); console.log("ENTER") }}
+            // onMouseLeave={() => { setHover(false); console.log("LEAVE") }}
+            >
                 <img className="sub-image" src={disneyPlus} />
                 <div className="sub-title">Disney Plus</div>
-            </div> :
-            
-                <div className="subcard--hover"
-                    // onMouseEnter={() => { setHover(true); console.log("ENTER") }}
-                    onMouseLeave={() => { setHover(false); console.log("LEAVE") }}>
-                    
-                    <div className="subcard--inner">
+            </div>
+
+            <div className="subcard--hover"
+            // onMouseEnter={() => { setHover(true); console.log("ENTER") }}
+            // onMouseLeave={() => { setHover(false); console.log("LEAVE") }}
+            >
+
+                <div className="subcard--inner">
                     <img className="sub-image hover" src={disneyPlus} />
 
                     <div className="card-topline">
@@ -38,12 +53,12 @@ function SubscriptionCardElement(props) {
 
 
                     <div className="sub-description">Endless entertainment from Disney, Pixar, Marvel, Star Wars, and National Geographic. From new releases, to your favorite classics, the past, present, and future are yours.</div>
-                    </div>
                 </div>
+            </div>
 
-            } 
 
-        </>
+
+        </div>
 
     )
 }
